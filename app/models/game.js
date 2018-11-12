1720f21385591b56a89a782b3c31e7de29a50d9e
adjeielias90 <adjeielias90@gmail.com>
2018-11-12 16:59:19 +0000

    Add components, routes and templates Add program logic Add seed data

--- app/models/game.js
+++ app/models/game.js
@@ -1,9 +1,21 @@
 import DS from 'ember-data';
-
+import {computed} from '@ember/object';
 export default DS.Model.extend({
     homeTeam: DS.belongsTo('team',{ inverse: 'homeGames' }),
     awayTeam: DS.belongsTo('team', {inverse: 'awayGames' }),
     homeGoals: DS.attr('number'),
     awayGoals: DS.attr('number'),
-    playedOn: DS.attr('date')
+    playedOn: DS.attr('date'),
+    isDraw: computed('homeGoals', 'awayGoals', function(){
+        return this.homeGoals === this.awayGoals;
+    }),
+
+    isHomeWin: computed('homeGoals', 'awayGoals', function(){
+        return this.homeGoals > this.awayGoals;
+    }),
+
+    isAwayWin: computed('homeGoals', 'awayGoals', function(){
+        return this.homeGoals < this.awayGoals;
+    }),
+
 });
--- app/models/team.js
+++ app/models/team.js
@@ -1,7 +1,39 @@
 import DS from 'ember-data';
+import {union, filterBy, sum, mapBy} from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
     homeGames: DS.hasMany('game', {inverse: "homeTeam"}),
     awayGames: DS.hasMany('game', {inverse: "awayTeam"}),
+    games: union('homeGames', 'awayGames'),
+    gamesDrawn: filterBy('games', 'isDraw'),
+    homeGamesWon: filterBy('games', 'isHomeWin'),
+    awayGamesWon: filterBy('games', 'isAwayWin'),
+    gamesWon: union('homeGamesWon', 'awayGamesWon'),
+
+    homeGamesLost: filterBy('games', 'isAwayWin'),
+    awayGamesLost: filterBy('games', 'isHomeWin'),
+    gamesLost: union('homeGamesLost', 'awayGamesLost'),
+
+    homeGoalsScoredArray: mapBy('homeGames', 'homeGoals'),
+    homeGoalsScored: sum('homeGoalsScoredArray'),
+
+    awayGoalsScoredArray: mapBy('awayGames', 'awayGoals'),
+    awayGoalsScored: sum('awayGoalsScoredArray'),
+
+    goalsScored: computed('homeGoalsScored', 'awayGoalsScored', function(){
+        return this.homeGoalsScored + this.awayGoalsScored;
+    }),
+
+
+    homeGoalsConcededArray: mapBy('homeGames', 'awayGoals'),
+    homeGoalsConceded: sum('homeGoalsConcededArray'),
+
+    awayGoalsConcededArray: mapBy('awayGames', 'homeGoals'),
+    awayGoalsConceded: sum('awayGoalsConcededArray'),
+
+    goalsConceded: computed('homeGoalsConceded', 'awayGoalsConceded', function(){
+        return this.homeGoalsConceded + this.awayGoalsConceded;
+    })
 });
